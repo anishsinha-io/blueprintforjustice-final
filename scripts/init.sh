14ea157468755bb8556bb95b3c71cc50c765d3f0
Anish Sinha <anishsinha@anish.fios-router.home>
2022-08-15 16:46:18 -0400

    removed unnecessary files

--- scripts/init.sh
+++ scripts/init.sh
@@ -16,16 +16,15 @@ cd client
 echo "$(timestamp): SERVING PRODUCTION-OPTIMIZED CLIENT"
 npx serve -s build &
 cd ../server
-echo "$(timestamp): SEARCHING FOR `blueprintforjustice` VIRTUAL ENVIRONMENT"
-if { conda env list | grep 'blueprintforjustice'; } >/dev/null 2>&1; then
-   	conda activate blueprintforjustice
-else
-	conda create -n blueprintforjustice
-fi
-echo "$(timestamp): ACTIVATING VIRTUAL ENVIRONMENT"
-conda activate blueprintforjustice
 echo "$(timestamp): INSTALLING PACKAGES"
 pip install -r requirements.txt
+echo "$(timestamp): VERIFYING SQLITE"
+if test -f "/Users/anishsinha/Home/pythonprojects/blueprintforjustice-flask/v3/blueprintforjustice/server/db.sqlite3"; then
+    echo "$(timestamp): USING EXISTING DATABASE"
+else
+    echo "$(timestamp): DATABASE DOES NOT EXIST. RECREATING FROM MYSQL BACKUP"
+    /home/ubuntu/util/mysql2sqlite/mysql2sqlite /home/ubuntu/blueprintforjustice/server/create-links.sql | sqlite /home/ubuntu/blueprintforjustice/service/db.sqlite3
+fi
 echo "$(timestamp): STARTING gunicorn AND BINDING TO PORT 8888"
 gunicorn --bind 0.0.0.0:8888 -w 4 -t 60 wsgi:server &
 echo "$(timestamp): STARTING nginx"
