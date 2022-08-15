9a54aebcb8a3279db2725d5d2a335e351dcbeace
Anish Sinha <anishsinha@anish.fios-router.home>
2022-08-15 16:59:42 -0400

    updated READMEs

--- scripts/init.sh
+++ scripts/init.sh
@@ -17,24 +17,31 @@
 
 #! /bin/bash
 
+# production initialization script
 timestamp() {
    date +"%Y-%m-%d_%H-%M-%S"
 }
 
+# update packages
 sudo apt update
 echo "\n$(timestamp): STARTING INITIALIZATION"
 echo "$(timestamp): EXPORTING NVM VERSION"
+# make sure nvm exists
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 echo "$(timestamp): USING NODE VERSION 16.16.0 (LTS)"
+# use the LTS release of Node.js
 nvm use 16.16.0
 cd /home/ubuntu/BlueprintForJustice
 cd client
 echo "$(timestamp): SERVING PRODUCTION-OPTIMIZED CLIENT"
+# server the static build folder in the background
 npx serve -s build &
 cd ../server
 echo "$(timestamp): INSTALLING PACKAGES"
+# install all packages
 pip install -r requirements.txt
+# make sure that db.sqlite3 exists. if not, create it from the MYSQL backup file
 echo "$(timestamp): VERIFYING SQLITE"
 if test -f "/Users/anishsinha/Home/pythonprojects/blueprintforjustice-flask/v3/blueprintforjustice/server/db.sqlite3"; then
     echo "$(timestamp): USING EXISTING DATABASE"
@@ -43,10 +50,13 @@ else
     /home/ubuntu/util/mysql2sqlite/mysql2sqlite /home/ubuntu/blueprintforjustice/server/create-links.sql | sqlite /home/ubuntu/blueprintforjustice/service/db.sqlite3
 fi
 echo "$(timestamp): STARTING gunicorn AND BINDING TO PORT 8888"
+# run the server on port 8888 with a production WSGI server
 gunicorn --bind 0.0.0.0:8888 -w 4 -t 60 wsgi:server &
 echo "$(timestamp): STARTING nginx"
+# reload nginx if it's running, if not, start it
 if [ -e /var/run/nginx.pid ]; then sudo nginx -s reload
 else 
 	sudo nginx
 fi
 echo "$(timestamp): APPLICATION INITIALIZATION COMPLETE"
+# if we're here, everything should've started successfully
