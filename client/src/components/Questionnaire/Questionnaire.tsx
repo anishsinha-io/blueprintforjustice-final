84a3a9b1182f9d3281aa1efa6dec0fd16d3afc1e
anishsinha <anishsinha0128@gmail.com>
2022-08-19 18:39:22 -0400

    modified questionnaire and it  but I'll be honest this is some of the worst looking code I've ever written (see /client/components/Questionnaire/Questionnaire.tsx)

--- client/src/components/Questionnaire/Questionnaire.tsx
+++ client/src/components/Questionnaire/Questionnaire.tsx
@@ -16,33 +16,166 @@
  **/
 
 import SettingsCtx from "components/ctx";
-import { useContext, useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Button from "components/Reusables/Button";
 import Slide from "components/Questionnaire/Slide";
 
 import { setPromptQuestionnaire, getPromptQuestionnaire } from "components/ctx";
-import QContext, { QContextProps } from "components/Questionnaire/qctx";
+import { optionsList } from "components/Questionnaire/qctx";
 
 const Questionnaire = () => {
   useEffect(() => window.scrollTo(0, 0), []);
   const navigate = useNavigate();
   const ctx = useContext(SettingsCtx);
-  const qctx = useContext(QContext);
+  const [slide, setSlide] = useState<number>(0);
+  const [checked, setChecked] = useState<boolean[]>([false, false, false]);
+  const [done, setDone] = useState<boolean>(false);
+  const [disabledOthers, setDisabledOthers] = useState<boolean>(false);
+  const [scores, setScores] = useState<{
+    legal: any;
+    healing: any;
+    action: any;
+    community: any;
+    media: any;
+    general: any;
+  }>({
+    legal: 0,
+    healing: 0,
+    action: 0,
+    community: 0,
+    media: 0,
+    general: 0,
+  });
   const handleExit = () => {
     setPromptQuestionnaire(false);
     ctx.setPromptQuestionnaire(getPromptQuestionnaire());
     navigate("/");
   };
+
+  const handleNext = () => {
+    if (slide === 5) setDone(() => true);
+    setSlide(() => slide + 1);
+    setChecked(() => [false, false, false]);
+    setDisabledOthers(false);
+  };
+  const handlePrev = () => {
+    setSlide(() => slide - 1);
+  };
   return (
     <div className="app-questionnaire">
-      <Slide slideNum={1} />
+      {!done && (
+        <fieldset className="questionnaire-wrapper">
+          <legend>
+            <h2>Question {slide + 1 ?? "--"}</h2>
+          </legend>
+          <div className="questionnaire-wrapper__question">
+            <p>{optionsList[slide][0]}</p>
+          </div>
+          <div className={`option-checkbox ${ctx.darkmode ? "dark" : ""}`}>
+            <input
+              type="checkbox"
+              id="option1"
+              name="interest"
+              value="option1"
+              checked={checked[0]}
+              disabled={disabledOthers}
+              onChange={() => {
+                setChecked(() => [!checked[0], checked[1], checked[2]]);
+                setDisabledOthers(true);
+                setScores(() => ({
+                  ...scores,
+                  [optionsList[slide][2].category]:
+                    scores[
+                      `${optionsList[slide][2].category}` as
+                        | "legal"
+                        | "healing"
+                        | "media"
+                        | "general"
+                        | "action"
+                        | "community"
+                    ] + optionsList[slide][2].points,
+                }));
+                console.log(scores);
+              }}
+            />
+            <label htmlFor="option1">
+              {optionsList[slide][1].text ?? "--"}
+            </label>
+          </div>
+          <div className={`option-checkbox ${ctx.darkmode ? "dark" : ""}`}>
+            <input
+              type="checkbox"
+              id="option2"
+              name="interest"
+              value="option2"
+              checked={checked[1]}
+              disabled={disabledOthers}
+              onChange={() => {
+                setChecked(() => [checked[0], !checked[1], checked[2]]);
+                setDisabledOthers(true);
+                setScores(() => ({
+                  ...scores,
+                  [optionsList[slide][2].category]:
+                    scores[
+                      `${optionsList[slide][2].category}` as
+                        | "legal"
+                        | "healing"
+                        | "media"
+                        | "general"
+                        | "action"
+                        | "community"
+                    ] + optionsList[slide][2].points,
+                }));
+              }}
+            />
+            <label htmlFor="option2">
+              {optionsList[slide][2].text ?? "--"}
+            </label>
+          </div>
+          <div className={`option-checkbox ${ctx.darkmode ? "dark" : ""}`}>
+            <input
+              type="checkbox"
+              id="option3"
+              name="interest"
+              value="option3"
+              checked={checked[2]}
+              disabled={disabledOthers}
+              onChange={() => {
+                setChecked(() => [checked[0], checked[1], !checked[2]]);
+                setDisabledOthers(true);
+                setScores(() => ({
+                  ...scores,
+                  [optionsList[slide][2].category]:
+                    scores[
+                      `${optionsList[slide][2].category}` as
+                        | "legal"
+                        | "healing"
+                        | "media"
+                        | "general"
+                        | "action"
+                        | "community"
+                    ] + optionsList[slide][2].points,
+                }));
+              }}
+            />
+            <label htmlFor="option3">
+              {optionsList[slide][3].text ?? "--"}
+            </label>
+          </div>
+          <Button
+            className="btn btn-questionnaire questionnaire-next"
+            text="next"
+            onClick={handleNext}
+          />
+        </fieldset>
+      )}
       <Button
-        className="btn btn-exit"
-        text="Return Home"
+        className="btn btn-questionnaire questionnaire-exit"
+        text="exit"
         onClick={handleExit}
-      />
+      ></Button>
     </div>
   );
 };
--- client/src/components/Questionnaire/qctx.ts
+++ client/src/components/Questionnaire/qctx.ts
@@ -12,12 +12,13 @@
  ** GNU General Public License for more details.
  **
  ** You should have received a copy of the GNU General Public License
- ** along with this program.  If not, see http://www.gnu.org/licenses/.
+ ** along with this program. If not, see http://www.gnu.org/licenses/.
  **/
 
 import React from "react";
 
 export interface QContextProps {
+  slideNum: number;
   action: number;
   media: number;
   community: number;
@@ -27,6 +28,7 @@ export interface QContextProps {
 }
 
 const QContext = React.createContext<QContextProps>({
+  slideNum: 0,
   action: 0,
   media: 0,
   community: 0,
@@ -38,5 +40,76 @@ const QContext = React.createContext<QContextProps>({
 const getRecommendations = (qctx: QContextProps): string[] => {
   return [];
 };
+
+export const optionsList: any = [
+  [
+    "To what end are you seeking resources?",
+    {
+      text: "I, or someone I know has been affected by police brutality.",
+      category: "legal",
+      points: 2,
+    },
+    {
+      text: "My broader community has been affected by police brutality.",
+      category: "action",
+      points: 1,
+    },
+    {
+      text: "I am seeking more information for learning.",
+      category: "general",
+      points: 1,
+    },
+  ],
+  [
+    "Do you feel you might require legal aid?",
+    { text: "Yes", category: "legal", points: 2 },
+    { text: "No", category: "legal", points: 0 },
+    { text: "I'm not sure", category: "legal", points: 1 },
+  ],
+  [
+    "Will you need to handle ongoing media/press coverage?",
+    { text: "Yes, and I'd like to learn more", category: "media", points: 2 },
+    {
+      text: "No, there isn't any press coverage I need to handle",
+      category: "media",
+      points: 0,
+    },
+    {
+      text: "I'm not sure",
+      category: "media",
+      points: 1,
+    },
+  ],
+  [
+    "Are you interested in additional resources for research",
+    { text: "Yes", category: "general", points: 2 },
+    { text: "No", category: "general", points: 0 },
+    {
+      text: "I'm not sure",
+      category: "general",
+      points: 1,
+    },
+  ],
+  [
+    "<action question>",
+    { text: "Yes", category: "action", points: 2 },
+    { text: "No", category: "action", points: 0 },
+    {
+      text: "I'm not sure",
+      category: "action",
+      points: 1,
+    },
+  ],
+  [
+    "<community question>",
+    { text: "Yes", category: "community", points: 2 },
+    { text: "No", category: "community", points: 0 },
+    {
+      text: "I'm not sure",
+      category: "community",
+      points: 1,
+    },
+  ],
+];
 
 export default QContext;
